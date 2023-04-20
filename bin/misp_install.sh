ce68f5aaa91066ca39dd02df9072e2b38d5c05af
gianluz93 <79843488+gianluz93@users.noreply.github.com>
2023-04-20 17:24:01 +0200

    fixed findutils install

--- bin/misp_install.sh
+++ bin/misp_install.sh
@@ -74,7 +74,7 @@ chown apache:apache /var/www/MISP/app/attachments
 
 # File permission
 chown -R root:apache /var/www/MISP
-sudo dnf -y install findutils
+dnf -y install findutils
 find /var/www/MISP -type d -exec chmod g=rx '{}' \;
 #find /var/www/MISP -type d -exec chmod g=rx {} \;
 
